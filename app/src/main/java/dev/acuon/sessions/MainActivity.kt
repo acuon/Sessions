493bee7777a71101d1eb556fe0bb378f5fb22b83
Rohit Sharma <rohitshar8600@gmail.com>
2022-06-10 11:47:40 +0530

    session 1 completed

--- app/src/main/java/dev/acuon/sessions/MainActivity.kt
+++ app/src/main/java/dev/acuon/sessions/MainActivity.kt
@@ -2,10 +2,45 @@ package dev.acuon.sessions
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    override fun onStart() {
+        super.onStart()
+        toast("onStart")
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        toast("onCreate")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        toast("onResume")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        toast("onRestart")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        toast("onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        toast("onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        toast("onDestroy")
+    }
+    private fun toast(str: String) {
+        Toast.makeText(this, "$str started", Toast.LENGTH_SHORT).show()
     }
 }
