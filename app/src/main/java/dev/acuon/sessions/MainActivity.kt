efd0ffdf07e9d76046e47a081e0051bb3e8be628
Rohit Sharma <rohitshar8600@gmail.com>
2022-06-17 02:15:23 +0530

    initial commit

--- app/src/main/java/dev/acuon/sessions/MainActivity.kt
+++ app/src/main/java/dev/acuon/sessions/MainActivity.kt
@@ -2,10 +2,13 @@ package dev.acuon.sessions
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import dev.acuon.sessions.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
